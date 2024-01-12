1004d1331712969abc26e88bd7328151aac93525
riskyyy <theloneranger241@gmail.com>
2024-01-12 14:37:55 +0700

    Cập nhật link

--- resources/views/home-layouts/components/recommend-list.blade.php
+++ resources/views/home-layouts/components/recommend-list.blade.php
@@ -23,7 +23,7 @@
                 @foreach($hotels as $hotel)
                     <div class="swiper-slide add-hotel-cart" data-hotel="{{$hotel->id}}">
 
-                        <a href="{{--{{route('home.hotel.show',$hotel)}}--}}#" class="hotelsCard -type-1 ">
+                        <a href="{{route('home.hotel.show',$hotel)}}" class="hotelsCard -type-1 ">
                             <div class="hotelsCard__image">
 
                                 <div class="cardImage ratio ratio-1:1">
